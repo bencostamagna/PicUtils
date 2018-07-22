9b24f53b4784d861308cbdd3744abe64886e153d
Benoit <b.costamagna@protonmail.com>
2018-07-22 08:51:23 +0200

    Adding help message, dry run option and mime type detection

--- compress.sh
+++ compress.sh
@@ -2,7 +2,22 @@
 
 
 usage() {
-	echo "Usage: ./compress [-r] foldernames"
+	echo "Usage: 	$0 [options] foldernames"
+	echo "	$0 --help to list options"
+}
+
+print_help() {
+	echo "Usage: 	$0 [options] foldernames"
+	echo ""
+	echo "Options"
+	echo ""
+	echo "	-h  | --help		Prints this help message"
+	echo "	-r  | -R		Recursive"
+	echo "	-dr | --dry-run		Tests file compression but keeps files unchanged on disk"
+	echo ""
+
+
+
 }
 
 
@@ -10,14 +25,21 @@ process_folder() {
 	echo "Converting Files in $1"
 	for file in "$1"/*
 	do
-		if [ -f "$file" ]
+		if [ -f "$file" ] && [ $(file --mime-type -b "$file") = "image/jpeg" ]
 		then
-			if [ "$(stat -c%s "$file")" -ge 5000000 ]
+			quality=$(identify -format '%Q' "$file")
+			if [ $quality > $QUALITY_TARGET ]
 			then
 				oldsize=$(du -h "$file" | cut -f1)
-				convert -quality 85 "$file" "$file"
-				newsize=$(du -h "$file" | cut -f1)
-				echo "Compressing file $file: $oldsize -> $newsize"
+				convert -quality $QUALITY_TARGET "$file" "$file.tmp"
+				newsize=$(du -h "$file.tmp" | cut -f1)
+				echo "	Compressing file $file: quality: $quality -> $QUALITY_TARGET, size: $oldsize -> $newsize"
+				if [ $DO_NOTHING = true ]
+				then
+					rm "$file.tmp"
+				else
+					mv "$file.tmp" "$file"
+				fi
 			fi
 		elif [ $RECURSIVE = true ] && [ -d "$file" ]
 		then
@@ -29,18 +51,28 @@ process_folder() {
 if [ $# -eq 0 ]
 then
 	usage
+	exit 1
 fi
 
 RECURSIVE=false
+DO_NOTHING=false
+QUALITY_TARGET=85
 while true;
 do
 	case "$1" in
     		-r | -R ) RECURSIVE=true; shift ;;
+    		-dr | --dry-run ) DO_NOTHING=true; shift ;;
+    		-h | --help ) PRINT_HELP=true; shift ;;
     		-- ) shift; break ;;
     		* ) break ;;
   	esac
 done
 
+if [ $PRINT_HELP = true ]
+then
+	print_help
+	exit 0
+fi
 
 
 for dir in "$@"
