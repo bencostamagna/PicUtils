c9c53eec6af6b493304c62907473218fbca314c3
Benoit <b.costamagna@protonmail.com>
2018-07-22 06:54:37 +0200

    Adding support for nested folders through -r option

--- compress.sh
+++ compress.sh
@@ -1,24 +1,53 @@
 #! /bin/bash
 
 
+usage() {
+	echo "Usage: ./compress [-r] foldernames"
+}
+
+
+process_folder() {
+	echo "Converting Files in $1"
+	for file in "$1"/*
+	do
+		if [ -f "$file" ]
+		then
+			if [ "$(stat -c%s "$file")" -ge 5000000 ]
+			then
+				oldsize=$(du -h "$file" | cut -f1)
+				convert -quality 85 "$file" "$file"
+				newsize=$(du -h "$file" | cut -f1)
+				echo "Compressing file $file: $oldsize -> $newsize"
+			fi
+		elif [ $RECURSIVE = true ] && [ -d "$file" ]
+		then
+			process_folder "$file"
+		fi
+	done
+}
+
+if [ $# -eq 0 ]
+then
+	usage
+fi
+
+RECURSIVE=false
+while true;
+do
+	case "$1" in
+    		-r | -R ) RECURSIVE=true; shift ;;
+    		-- ) shift; break ;;
+    		* ) break ;;
+  	esac
+done
+
+
+
 for dir in "$@"
 do
 	if [ -d "$dir" ]
 	then
-		echo "Converting Files in $dir"
-		for file in "$dir"/*
-		do
-			if [ -f "$file" ]
-			then
-				if [ "$(stat -c%s "$file")" -ge 5000000 ]
-				then
-					oldsize=$(du -h "$file" | cut -f1)
-					convert -quality 85 "$file" "$file"
-					newsize=$(du -h "$file" | cut -f1)
-					echo "Compressing file $file: $oldsize -> $newsize"
-				fi
-			fi
-		done
+		process_folder $dir
 	fi
 done
 
